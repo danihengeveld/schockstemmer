d7b06247f893912013684122d337bc2a553bd4b2
Dani Hengeveld <dani.hengeveld@infosupport.com>
2025-12-17 09:20:46 +0100

    changes

--- src/components/new-game/create-game-button.tsx
+++ src/components/new-game/create-game-button.tsx
@@ -6,10 +6,9 @@ import { useMutation } from "convex/react";
 export default function CreateGameButton() {
   const createGame = useMutation(api.games.createGame)
 
-  const handleCreateGame = () => {
-    createGame({}).then((result) => {
-      console.log("Created game with ID:", result.gameId)
-    })
+  const handleCreateGame = async () => {
+    const game = await createGame();
+    console.log("Created game:", JSON.stringify(game));
   }
 
   return (
